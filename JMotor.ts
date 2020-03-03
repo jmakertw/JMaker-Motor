b45c946463c7e554d246261aeeb3a727e8dd56ca
傑森創工 JMaker Workshop <jason080@gmail.com>
2020-03-03 14:25:03 +0800

    Update JMotor.ts

--- JMotor.ts
+++ JMotor.ts
@@ -22,7 +22,7 @@ let S0_PIN = AnalogPin.P0;
 let S1_PIN = AnalogPin.P1;
 let S2_PIN = AnalogPin.P2;
 
-//% color=204 weight=0 
+//% color=204
 //% icon="\uf63b"
 //% block="jBit Car"
 namespace JMotor {
